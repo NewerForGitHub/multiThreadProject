2e10a3fb7ef8f7d0f035121619daf889b16949dd
zhengchaoqun <zhengchaoqun@ucredit.com>
2017-09-21 15:31:07 +0800

    代码提交

--- src/testThread/myPriority/MyThread1.java
+++ src/testThread/myPriority/MyThread1.java
@@ -4,17 +4,16 @@ import java.util.Random;
 
 public class MyThread1 extends Thread {
 
-	@Override
-	public void run() {
-		long start = System.currentTimeMillis();
-		long addResult = 0;
-		for(int j=0; j<10; j++) {
-			for(int i=0; i<50000; i++) {
-				addResult += new Random().nextInt();
-			}
-		}
-		long end = System.currentTimeMillis();
-		System.out.println("¡ï¡ï¡ï¡ï¡ï Thread 1 use time = " + (end - start));
-	}
+    @Override
+    public void run() {
+        long start = System.currentTimeMillis();
+        for (int j = 0; j < 10; j++) {
+            for (int i = 0; i < 50000; i++) {
+                new Random().nextInt();
+            }
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("¡ï¡ï¡ï¡ï¡ï Thread 1 use time = " + (end - start));
+    }
 
 }
